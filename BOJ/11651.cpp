b4bd4fcbb9e44c0476550e2f3838de6263cdb9a8
gtd9511 <ydj02109@naver.com>
2023-08-09 19:52:43 +0900

    230809 C03 sort ing

---
+++ BOJ/11651.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int	N;
+vector<pair<int, int> >	v;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+
+	while (N--)
+	{
+		int x, y;
+		cin >> y >> x;
+		v.push_back(make_pair(x, y));
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i].second << ' ' << v[i].first << '\n';
+
+	return (0);
+}
