93a4b2bcd53efd1402ac2dab6a4e70fc0f6cc391
gtd9511 <ydj02109@naver.com>
2023-08-12 22:28:22 +0900

    230812 M00 10816

---
+++ BOJ/10816.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+
+int main()
+{
+	vector<int> v;
+
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+	sort(v.begin(), v.end());
+	cin >> M;
+	for (int i = 0; i < M; i++)
+	{
+		int temp;
+		cin >> temp;
+		cout << upper_bound(v.begin(), v.end(), temp) - lower_bound(v.begin(), v.end(), temp) << " ";
+	}
+	return (0);
+}
