bbd25e0d1b0818e3f5df4fb282ede4a31ded578c
gtd9511 <ydj02109@naver.com>
2023-08-19 12:09:58 +0900

    230819 BOJ 4949

--- BOJ/4949.cpp
+++ BOJ/4949.cpp
@@ -8,57 +8,49 @@ int main()
 	while(1)
 	{
 		string str;
-		cin >> str;
-		stack<int> s1;
-		stack<int> s2;
-		int flag1 = 0;
-		int flag2 = 0;
-		if (str[0] == '.' && str.length() == 1)
+		getline(cin, str, '.');
+		stack<int> s;
+		bool flag = false;
+
+		if (str.length() == 0)
 			break;
 		else
 		{
-
 			int i = 0;
 			for (int i = 0; i < str.length(); i++)
 			{
 				if (str[i] == '(')
-				{
-					s1.push(1);
-					flag1 = 1;
-					if (flag2 == 1)
-						flag2 = 2;
-				}
+					s.push(1);
 				else if (str[i] == '[')
+					s.push(2);
+				else if (str[i] == ')')
 				{
-					s2.push(1);
-					flag2 = 1;
-					if (flag1 == 1)
-						flag1 = 2;
+					if (s.empty())
+						flag = true;
+					else if (s.top() == 1)
+						s.pop();
+					else
+						flag = true;
 				}
-				else if (str[i] == ')' && flag1 == 1)
+				else if (str[i] == ']')
 				{
-					s1.pop();
-					flag1 = 0;
+					if (s.empty())
+						flag = true;
+					else if (s.top() == 2)
+						s.pop();
+					else
+						flag = true;
 				}
-				else if (str[i] == ']' && flag2 == 1)
-				{
-					s2.pop();
-					flag2 = 0;
-				}
-				// cout << "s1 size : " << s1.size() << endl;
-				// cout << "s2 size : " << s2.size() << endl;
-				// cout << "flag 1 : " << flag1 << endl;
-				// cout << "flag 2 : " << flag2 << endl;
+				// cout << "str[i] : " << str[i] << " statck size : " << s.size() << endl;
 			}
-			// cout << "s1 size : " << s1.size() << endl;
-			// cout << "s2 size : " << s2.size() << endl;
-			// cout << "flag 1 : " << flag1 << endl;
-			// cout << "flag 2 : " << flag2 << endl;
+			cin.ignore();
 		}
-		if (s1.size() || s2.size() || flag1 || flag2)
+		if (s.size() != 0 || flag)
 			cout << "no\n";
 		else
 		 	cout << "yes\n";
+		while (!s.empty())
+			s.pop();
 	}
 	return (0);
 }
