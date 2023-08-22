f6da8e1dd86fdcf805fcd920dfb61e34d4f0ded9
gtd9511 <ydj02109@naver.com>
2023-08-22 16:24:01 +0900

    230822 BOJ 1918

--- BOJ/1918.cpp
+++ BOJ/1918.cpp
@@ -1,10 +1,67 @@
 #include <iostream>
-#include <deque>
+#include <stack>
 using namespace std;
 
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	stack<char> op;
+
 	string str;
 	cin >> str;
 
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			cout << str[i];
+		if (str[i] == '(')
+			op.push(str[i]);
+		if (str[i] == '*' || str[i] == '/')
+		{
+			while (!op.empty() && (op.top() == '*' || op.top() == '/'))
+			{
+				cout << op.top();
+				op.pop();
+			}
+			op.push(str[i]);
+		}
+		if (str[i] == '+' || str[i] == '-')
+		{
+			while (!op.empty() && op.top() != '(')
+			{
+				cout << op.top();
+				op.pop();
+			}
+			op.push(str[i]);
+		}
+		if (str[i] == ')')
+		{
+			while (!op.empty() && op.top() != '(')
+			{
+				cout << op.top();
+				op.pop();
+			}
+			op.pop();
+		}
+	}
+	while (!op.empty())
+	{
+		cout << op.top();
+		op.pop();
+	}
+	return (0);
 }
+
+// + -
+// * /
+// ()
+
+//	A + B * C + D	ABC*+D+
+//	A * B + C * D	AB*CD*+
+//
+//
+//
+
