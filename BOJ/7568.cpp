76683475813c99e3542e00c9641d6947125de1a0
gtd9511 <ydj02109@naver.com>
2023-08-24 14:58:54 +0900

    230824 BOJ 7568

--- BOJ/7568.cpp
+++ BOJ/7568.cpp
@@ -2,27 +2,24 @@
 using namespace std;
 
 int N;
+pair<int, int> arr[50];
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
+
 	cin >> N;
-	while (N--)
+	for (int i = 0; i < N; i++)
+		cin >> arr[i].first >> arr[i].second;
+	for (int i = 0; i < N; i++)
 	{
-		int weight, height;
-		cin >> weight >> height;
-
-
+		int rank = 1;
+		for (int j = 0; j < N; j++)
+			if (arr[i].first < arr[j].first && arr[i].second < arr[j].second)
+				rank++;
+		cout << rank << ' ';
 	}
-
 	return (0);
 }
-
-
-// 4 2
-// 3 3
-// 1 1
-// 2 4
-// 5 5
