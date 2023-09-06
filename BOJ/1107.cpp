8b04a40939417dfa1443b0d0e364073bada01842
gtd9511 <ydj02109@naver.com>
2023-09-06 22:00:49 +0900

    230906 BOJ 1107ing

---
+++ BOJ/1107.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int arr[10] = {0, };
+int ans = 0;
+
+int main()
+{
+	int N, M;
+	cin >> N >> M;
+	for (int i = 0; i < M; i++)
+		cin >> arr[i];
+
+
+	return (0);
+}
