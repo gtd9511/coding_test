a47da07c69acacee0ab0060c808e912bd45e13db
gtd9511 <ydj02109@naver.com>
2023-09-08 00:23:44 +0900

    230908 BOJ 1107

--- BOJ/1107.cpp
+++ BOJ/1107.cpp
@@ -1,150 +1,72 @@
 #include <iostream>
 #include <cmath>
 #include <algorithm>
-#include <iterator>
 #include <vector>
+#include <string>
 using namespace std;
 
-int arr[10] = {0, };
-int current_ch = 100;
-int p_ans = 0;
-int m_ans = 0;
+bool use[10] = {0, };
+int p_current, m_current, p_ans = 0, m_ans = 0;
+
 vector<int> res;
 
-bool check_ch(int now)
+bool check_num(int num)
 {
-	// cout << "now : " << now << endl;
-
-	// if (now == 0 && arr[0] == -1)
-	// 	return false;
-	// else if (now == 0 && arr[0] == 0)
-	//  	return true;
-	while (now > 0)
-	{
-		int temp_single = now % 10;
-		// cout << "temp_single : " << temp_single << endl;
-		if (arr[temp_single] == -1)
-		{
-			// cout << "No" << endl;
-			return false;
-		}
-		now /= 10;
-	}
-	// cout << "YES" << endl;
-	return true;
+	string temp_str = to_string(num);
+	for (int i = 0; i < temp_str.length(); i++)
+		if (use[temp_str[i] - '0'] == 1)
+			return (false);
+	return (true);
 }
 
 int main()
 {
-	for (int i = 0; i < 10; i++)
-		arr[i] = i;
 	int N, M;
 	cin >> N >> M;
-
-
 	for (int i = 0; i < M; i++)
 	{
 		int temp;
 		cin >> temp;
-		arr[temp] = -1;
+		use[temp] = 1;
 	}
-
-	if (N == 100)
-	{
-		cout << 0;
-		return (0);
-	}
-	if (N == 0)
-	{
-		int temp = 0;
-		for (int i = 0; i < 10; i++)
-		{
-			if (arr[i] != -1)
-				temp = i + 1;
-		}
-		if (temp == 0)
-			cout << 100;
-		else
-		 	cout << temp;
-		return (0);
-	}
-	int no_num = 0;
-	no_num = abs(100 - N);
-	int plus_case = N;
-	int minus_case = N;
-	while (!check_ch(plus_case++))
+	res.push_back(abs(N - 100));
+	// cout << "no_num : " << res[0] << endl;
+	p_current = N;
+	m_current = N;
+	while (!check_num(p_current++))
 	{
 		p_ans++;
-		// cout << "p_ans : " << p_ans << endl;
-		if (plus_case > 1000000)
+		if (p_current > 1000000)
 		{
 			p_ans = 500000;
 			break;
 		}
 	}
-	// cout << "----------------" << endl;
-
-	while (!check_ch(minus_case--))
+	while (!check_num(m_current--))
 	{
 		m_ans++;
-
-		// if (minus_case == 0)
-		// {
-		// 	int check = m_ans;
-		// 	for (int i = 0; i < 9; i++)
-		// 	{
-		// 		if (arr[i] != -1)
-		// 		{
-		// 			m_ans += i + 1;
-		// 			break;
-		// 		}
-		// 	}
-		// 	if (check != m_ans)
-		// 		break;
-		// }
-		// if (minus_case == 0 && arr[0] != -1)
-		// {
-		// 	m_ans++;
-		// 	break;
-		// }
-		// else if (minus_case == 0 && arr[0] == -1)
-		// {
-		// 	for (int i = 9; i > 0; i--)
-		// 	{
-		// 		if (arr[i] != -1)
-
-		// 	}
-		// 	break;
-		// }
-
-		// cout << "m_ans : " << m_ans << endl;
+		if (m_current < -1)
+		{
+			m_ans = 500000;
+			break;
+		}
+		// cout << "wm_ans : " << m_ans << endl;
 	}
-	// cout << "----------------" << endl;
-
-	int plus_length = 0;
-	int minus_length = 0;
-	while (plus_case > 0)
-	{
-		plus_case /= 10;
-		plus_length++;
-	}
-	while (minus_case > 0)
-	{
-		minus_case /= 10;
-		minus_length++;
-	}
-	p_ans += plus_length;
-	m_ans += minus_length;
-	res.push_back(no_num);
+	// cout << m_current << " " << m_ans << endl;
+	p_current--;
+	m_current++;
+	p_ans += to_string(p_current).length();
+	m_ans += to_string(m_current).length();
+	// cout << "l : " << p_current << " " << to_string(p_current).length() << endl;
+	// cout << "l : " << m_current << " " << to_string(m_current).length() << endl;
+	// cout << "p_ans : " << p_ans << endl;
+	// cout << "m_ans : " << m_ans << endl;
 	res.push_back(p_ans);
 	res.push_back(m_ans);
+
 	sort(res.begin(), res.end());
+
 	cout << res[0];
-	// if (p_ans > m_ans)
-	// 	cout << m_ans;
-	// else
-	// 	cout << p_ans;
 	return (0);
 }
 
-//https://www.acmicpc.net/board/view/31853
