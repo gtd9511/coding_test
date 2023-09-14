32da169bd4e254390890f77a2099b58338314cad
gtd9511 <ydj02109@naver.com>
2023-09-15 00:49:35 +0900

    230914 BOJ 1932

--- BOJ/1932.cpp
+++ BOJ/1932.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int n;
+int n, dp[500][500] = {0, }, ans = 0;
 int main()
 {
 	cin >> n;
-	for (int i = 0;i < n; i++)
+	if (n == 1)
 	{
-
+		cin >> dp[0][0];
+		cout << dp[0][0];
+		return (0);
 	}
-	
+	for (int i = 0;i < n; i++)
+		for (int j = 0; j < i + 1; j++)
+			cin >> dp[i][j];
+
+	for (int i = 1; i < n; i++)
+		for (int j = 0; j < i + 1; j++)
+		{
+			if (j == 0)
+				dp[i][j] += dp[i - 1][0];
+			else if (i == j)
+				dp[i][j] += dp[i - 1][j - 1];
+			else
+				dp[i][j] += max(dp[i - 1][j - 1], dp[i - 1][j]);
+			if (ans < dp[i][j])
+				ans = dp[i][j];
+		}
+	cout << ans;
 	return (0);
 }
