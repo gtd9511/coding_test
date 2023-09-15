9ff3f5463d435ecd55bc538288f4423472651797
gtd9511 <ydj02109@naver.com>
2023-09-15 17:34:28 +0900

    230913 BOJ 11444

---
+++ BOJ/11444.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+long long n;
+
+
+int main()
+{
+	cin >> n;
+
+	return (0);
+}
+
+
+// A % N + B % N 9 7 16 7 25 7 4 2 2
+//
+//1 1 1
+//2 1 1
+//3 2 1 1
+//4 3 1 1 1
+//5 5 1 1 1 1 1
+//6 8
+//7 
