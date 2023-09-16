efa90c19a6cfc317a14b4ca10edae31045a964d5
gtd9511 <ydj02109@naver.com>
2023-09-16 23:42:53 +0900

    230916 BOJ 16953

--- BOJ/16953.cpp
+++ BOJ/16953.cpp
@@ -2,27 +2,32 @@
 #include <deque>
 using namespace std;
 
-int A, B, cnt = 1;
+long long A, B, cnt = 1;
 
 void dfs(int n)
 {
-	deque<int> dq;
-	dq.push_back(n);
+	deque<pair<long long, long long> > dq;
+	dq.push_back(make_pair(n, cnt));
 	while (!dq.empty())
 	{
-		if (2 * dq.front() == B || dq.front() * 10 + 1 == B)
+		cnt = dq.front().second;
+
+		if (2 * dq.front().first == B || dq.front().first * 10 + 1 == B)
+		{
+			cnt++;
 			return;
-		if (2 * dq.front() < B )
-			dq.push_back(2 * dq.front());
-		if (dq.front() * 10 + 1 < B)
-			dq.push_back(dq.front() * 10 + 1);
-		
-		for (int i = 0; i < dq.size(); i++)
-			cout << dq.front() << " ";
-		cout << endl;
+		}
+		if (2 * dq.front().first < 1000000000)
+			dq.push_back(make_pair(2 * dq.front().first, cnt + 1));
+		if (dq.front().first * 10 + 1 < 1000000000)
+			dq.push_back(make_pair(dq.front().first * 10 + 1, cnt + 1));
+
+		// for (int i = 0; i < dq.size(); i++)
+			// cout << "[" << dq[i].first << ", " << dq[i].second << "] ";
+		// cout << endl;
 		dq.pop_front();
-		cnt ++;
 	}
+	cnt = -1;
 	return ;
 }
 
@@ -31,4 +36,5 @@ int main()
 	cin >> A >> B;
 	dfs(A);
 	cout << cnt;
+	return (0);
 }
