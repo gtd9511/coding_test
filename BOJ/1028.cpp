eec264b747c573b8365c050f0e8cfcf017d5eb11
gtd9511 <ydj02109@naver.com>
2023-09-22 15:44:35 +0900

    230922 BOJ

--- BOJ/1028.cpp
+++ BOJ/1028.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int R, C, ans = 0;
-int dpleft[750][750] = {0, };
-int dpright[750][750] = {0, };
+long long dpleft[750][750] = {0, };
+long long dpright[750][750] = {0, };
 
 int main()
 {
@@ -28,55 +28,52 @@ int main()
 		}
 	}
 
-	for(int i = 0; i < R - 1; i++)
+	for(int i = 1; i < R; i++)
 	{
 		for(int j = 0; j < C; j++)
 		{
-			if(dpleft[i][j] != 0 && j != 0)
-				dpleft[i + 1][j - 1] += dpleft[i][j];
-			if(dpright[i][j] != 0 && j != C - 1)
-				dpright[i + 1][j + 1] += dpright[i][j];
+			if(dpleft[i][j] == 1 && j != C - 1)
+				dpleft[i][j] += dpleft[i - 1][j + 1];
+			if(dpright[i][j] == 1 && j != 0)
+				dpright[i][j] += dpright[i - 1][j - 1];
 		}
 	}
 
-	// for (int i = 0; i < R; i++)
+	// for(int i = 0; i < R; i++)
 	// {
-	// 	for (int j = 0; j < C; j++)
+	// 	for(int j = 0; j < C; j++)
 	// 		cout << dpleft[i][j] << " ";
 	// 	cout << endl;
 	// }
 	// cout << endl;
-	// for (int i = 0; i < R; i++)
+	// for(int i = 0; i < R; i++)
 	// {
-	// 	for (int j = 0; j < C; j++)
+	// 	for(int j = 0; j < C; j++)
 	// 		cout << dpright[i][j] << " ";
 	// 	cout << endl;
 	// }
-	for (int i = 0; i < R - 1; i++)
+
+	for (int i = R - 1; i >= 0; i--)
 	{
-		for (int j = 0; j < C; j++)
+		for (int j = C - 1; j >= 0; j--)
 		{
 			if (dpleft[i][j] != 0 && dpright[i][j] != 0)
 			{
-				ans = 1;
+				ans = max(ans, 1);
 				if (dpleft[i][j] != 1 && dpright[i][j] != 1)
-				// {
-				// 	int n = min(dpleft[i][j], dpright[i][j]);
-				// 	while (n > 1)
-				// 	{
-				// 		if (dpright[i + n - 1][j - n + 1] >= n && dpleft[i + n - 1][j + n - 1] >= n)
-				// 			ans = max(ans, n);
-				// 		n--;
-				// 	}
-
-
+				{
+					int n = min(dpleft[i][j], dpright[i][j]);
+					while (n > 1)
+					{
+						if (dpright[i - n + 1][j + n - 1] >= n && dpleft[i - n + 1][j - n + 1] >= n)
+							ans = max(ans, n);
+						n--;
+					}
 				}
 			}
 		}
 	}
-	if (ans != 0)
-		cout << ans + 1;
-
+	cout << ans;
 	return (0);
 }
 
