adaedb4348691a3d80616607660d6c055fd34054
gtd9511 <ydj02109@naver.com>
2023-09-26 20:01:04 +0900

    230926 BOJ 1238ing

--- BOJ/2473.cpp
+++ BOJ/2473.cpp
@@ -3,9 +3,9 @@
 #include <algorithm>
 using namespace std;
 
-int N;
-vector<long long> negative;
-vector<long long> positive;
+int N, ans1, ans2, ans3;
+vector<long long> num;
+long long triple = 3000000000;
 
 int main()
 {
@@ -14,56 +14,29 @@ int main()
 	{
 		int temp;
 		cin >> temp;
-		if (temp >= 0)
-			positive.push_back(temp);
-		else
-			negative.push_back(temp);
+		num.push_back(temp);
 	}
-	sort(positive.begin(), positive.end());
-	sort(negative.begin(), negative.end());
-	reverse(negative.begin(), negative.end());
+	sort(num.begin(), num.end());
 
-	long long triplepos = 3000000000, tripleneg = 3000000000 , triplecol = 3000000000;
-	if (positive.size() >= 3)
-		triplepos = positive[0] + positive[1] + positive[2];
-	if (negative.size() >= 3)
-		tripleneg = negative[0] + negative[1] + negative[2];
-
-	int p = 0, n = 0, ansp = 0, ansn = 0;
-	if (positive.size() && negative.size())
+	for (int i = 0; i < N - 2; i++)
 	{
-		while (p < positive.size() || n < negative.size())
+		int l = i + 1, r = N - 1;
+		while (l < r)
 		{
-			long long temp = positive[p] + negative[n];
-			if (abs(temp) < abs(triplecol) && p < positive.size() && n < negative.size())
+			long long temptriple = num[i] + num[l] + num[r];
+			if (abs(temptriple) < triple)
 			{
-				triplecol = temp;
-				ansp = p;
-				ansn = n;
+				triple = abs(temptriple);
+				ans1 = num[i];
+				ans2 = num[l];
+				ans3 = num[r];
 			}
-			if (temp > 0)
-			{
-				if (n < negative.size() - 1)
-					n++;
-				else if(p < positive.size())
-				 	p++;
-			}
+			if (temptriple < 0)
+				l++;
 			else
-			{
-				if (p < positive.size() - 1)
-					p++;
-				else if (n < negative.size())
-				 	n++;
-			}
+			 	r--;
 		}
 	}
-
-	if (abs(triplepos) < abs(tripleneg) && abs(triplepos) < abs(triplecol))
-		cout << positive[0] << " " << positive[1];
-	if (abs(tripleneg) < abs(triplepos) && abs(tripleneg) < abs(triplecol))
-		cout << negative[1] << " " << negative[0];
-	if (abs(triplecol) <= abs(tripleneg) && abs(triplecol) <= abs(triplepos))
-		cout << negative[ansn] << " " << positive[ansp];
-
+	cout << ans1 << " " << ans2 << " " << ans3;
 	return (0);
 }
